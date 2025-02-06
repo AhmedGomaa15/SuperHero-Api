c6efb980f0af6364bb135b8c302a71dfa961cb5b
Ahmed Mohamed Gomaa <99025430+AhmedGomaa15@users.noreply.github.com>
2025-02-06 15:49:13 +0200

    Add project files.

---
+++ SuperHero-Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHero_Api.Entities;
+
+namespace SuperHero_Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
---
+++ SuperHero-Api/Entities/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHero_Api.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty ;
+        public string Place { get; set; } = string.Empty;
+
+    }
+}
